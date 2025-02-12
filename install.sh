c8c29a9bdac1d5b6d5d785e05635a451561d79c5
VV01T3K <wojteks.access@gmail.com>
2025-02-12 01:50:32 +0000

    fix: update install script to  stop user-specific paths

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
-. /home/q/.nix-profile/etc/profile.d/nix.sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --update --extra-substituters https://cache.nixos.org
@@ -19,10 +19,10 @@ nix --extra-substituters https://cache.nixos.org profile install nixpkgs#direnv
 nix-collect-garbage -d
 nix-store --optimise
 
-echo 'eval "$(direnv hook zsh)"' >> /etc/zsh/zshrc
-echo 'export DIRENV_LOG_FORMAT=""' >> /etc/zsh/zshrc
-echo 'eval "$(direnv hook bash)"' >> /etc/bash.bashrc
-echo 'export DIRENV_LOG_FORMAT=""' >> /etc/bash.bashrc
+echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+echo 'export DIRENV_LOG_FORMAT=""' >> ~/.zshrc
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+echo 'export DIRENV_LOG_FORMAT=""' >> ~/.bashrc
 
-echo "echo 'Hello! Welcome to your dev environment 👋'" >> /etc/zsh/zshrc
-echo "echo 'Hello! Welcome to your dev environment 👋'" >> /etc/bash.bashrc
+echo "echo 'Hello! Welcome to your dev environment 👋'" >> ~/.zshrc
+echo "echo 'Hello! Welcome to your dev environment 👋'" >> ~/.bashrc
