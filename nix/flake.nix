2567f74dcf117774786e14f4b903cd22826418d7
VV01T3K <wojteks.access@gmail.com>
2025-02-18 01:13:37 +0000

    flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    bun-canary-src = {
+      url = "https://raw.githubusercontent.com/VV01T3K/nix-container/refs/heads/nix-bun-cannary/bun-canary.nix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, bun-canary-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        bun-canary = pkgs.callPackage ./bun-canary.nix {};
+        bun-canary = pkgs.callPackage (builtins.readFile bun-canary-src) {};
       in
       with pkgs; {
         devShells.default = mkShell {
